042fc67bbaab5066abdb7b34649e0311d257976d
Raphael <raphael@openhdfpv.org>
2025-06-29 19:35:10 +0200

    ssl

--- .github/workflows/ssl.ps1
+++ .github/workflows/ssl.ps1
@@ -1,15 +1,6 @@
-$version = "3_5_0"
-$installerName = "Win32OpenSSL-$version.exe"
-$installerUrl = "https://slproweb.com/download/$installerName"
-$installerPath = "$env:TEMP\$installerName"
+$zipUrl = "https://github.com/ohadravid/mingw-openssl/releases/download/2023-04-11/openssl-mingw-3.0.8-x86.zip"
+$zipPath = "$env:TEMP\openssl-mingw.zip"
 $installDir = "C:\OpenSSL32"
 
-Write-Host "Downloading OpenSSL installer from $installerUrl"
-Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
-
-Write-Host "Installing OpenSSL to $installDir..."
-$args = "/silent", "/sp-", "/suppressmsgboxes", "/DIR=`"$installDir`""
-Start-Process -FilePath $installerPath -ArgumentList $args -Wait
-
-$env:Path = "$installDir\bin;$env:Path"
-Write-Host "OpenSSL installed to $installDir"
+Invoke-WebRequest -Uri $zipUrl -OutFile $zipPath
+Expand-Archive -Path $zipPath -DestinationPath $installDir -Force
