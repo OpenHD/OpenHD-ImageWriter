00aaf35a55e3518d3db73303d07011b4fe151b21
Raphael <raphael@openhdfpv.org>
2025-06-29 17:35:48 +0200

    Update ssl.ps1

--- .github/workflows/ssl.ps1
+++ .github/workflows/ssl.ps1
@@ -1,52 +1,18 @@
-################################################################################
-##  File:  Install-OpenSSL.ps1
-##  Desc:  Install win64-openssl.
-##  Supply chain security: checksum validation
-################################################################################
+# Install OpenSSL 32-bit from Shining Light Productions
+$version = "3.1.4"
+$installerName = "Win32OpenSSL-$version.exe"
+$installerUrl = "https://slproweb.com/download/$installerName"
+$installerPath = "$env:TEMP\$installerName"
+$installDir = "C:\OpenSSL32"
 
-$arch = 'INTEL'
-$bits = '32'
-$light = $false
-$installer = "exe"
-$version = (Get-ToolsetContent).openssl.version
-$installDir = "$Env:ProgramFiles\OpenSSL"
+Write-Host "Downloading OpenSSL installer..."
+Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
 
-# Fetch available installers list
-$jsonUrl = 'https://raw.githubusercontent.com/slproweb/opensslhashes/master/win32_openssl_hashes.json'
+Write-Host "Installing OpenSSL to $installDir..."
+$args = "/silent", "/sp-", "/suppressmsgboxes", "/DIR=`"$installDir`""
+Start-Process -FilePath $installerPath -ArgumentList $args -Wait
 
-$installersAvailable = (Invoke-RestMethod $jsonUrl).files
+# Add to PATH for current session
+$env:Path = "$installDir\bin;$env:Path"
 
-$distributor_file_hash = $null
-$installerUrl = $null
-$installerName = $null
-
-$installersAvailable | Get-Member -MemberType NoteProperty | ForEach-Object {
-  $key = $_.Name
-  if(($installersAvailable.$key.light -eq $light) -and ($installersAvailable.$key.arch -eq $arch) -and ($installersAvailable.$key.bits -eq $bits) -and ($installersAvailable.$key.installer -eq $installer) -and ($installersAvailable.$key.basever -eq $version)) {
-    $installerUrl = $installersAvailable.$key.url
-    $installerName = $key
-    $distributor_file_hash = $installersAvailable.$key.sha512
-  }
-}
-
-# Invoke installation
-
-$installerArgs = '/silent', '/sp-', '/suppressmsgboxes', "/DIR=`"$installDir`""
-Install-Binary -Url "$installerUrl" -Name "$installerName" -ArgumentList $installerArgs
-
-#region Supply chain security
-Write-Verbose "Performing checksum verification"
-$local_file_hash = (Get-FileHash -Path (Join-Path ${env:TEMP} $installerName) -Algorithm SHA512).Hash
-
-if ($local_file_hash -ne $distributor_file_hash) {
-        Write-Host "hash must be equal to: ${distributor_file_hash}"
-        Write-Host "actual hash is: ${local_file_hash}"
-        throw 'Checksum verification failed, please rerun install'
-}
-#endregion
-
-# Update PATH
-Add-MachinePathItem "$installDir\bin"
-$env:Path = Get-MachinePath
-
-Invoke-PesterTests -TestFile "Tools" -TestName "OpenSSL"
+Write-Host "OpenSSL installed successfully to $installDir"
